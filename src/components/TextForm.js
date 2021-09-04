50e4cec07fc8a1952ae9c1c0aa9d1e1962e918f5
Akashdeepgupta <akashdeepg2001@gmail.com>
2021-09-04 12:05:53 +0530

    lowercase button and preview

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -11,18 +11,34 @@ export default function TextForm(props) {
         setText(newText);
     }
 
+    const handleLoClick = () => {
+        console.log("lower case was clicked");
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
+
     const handleOnChange =(event) =>{
         console.log("handle change")
         setText(event.target.value);
     }
     return (
-        <div>
+        <>
+          <div className="container"> 
             <h1>{props.heading}</h1>
             <div className="mb-3">
             <textarea className="form-control"  value={text} onChange={handleOnChange} id="myBox" rows="8" ></textarea>
             </div>
             <button className="btn btn-primary" onClick={handleUpClick} >convert to uppercase</button>
-            <button className="btn btn-primary" onClick={handleUpClick} >convert to Lowercase</button>
-        </div>
+            <button className="btn btn-primary mx-2" onClick={handleLoClick} >convert to Lowercase</button>
+         </div>
+         <div className="container my-3">
+             <h1>Your Text summary</h1>
+             <p>{text.split(" ").length} words and {text.length} characters</p>
+             <p>{0.008*text.split(" ").length} minutes read</p>
+             <h2>Preview</h2>
+             <p>{text}</p>
+         </div>
+
+        </>
     )
 }
