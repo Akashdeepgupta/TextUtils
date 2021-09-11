8e40207d9d1ccf0f533758ab90a7b0bcde47c851
Akashdeepgupta <akashdeepg2001@gmail.com>
2021-09-11 20:00:46 +0530

    11_sept

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -17,6 +17,25 @@ export default function TextForm(props) {
         setText(newText);
     }
 
+
+    const handleclrClick = () => {
+        console.log("clear text");
+        let newText = "";
+        setText(newText);
+    }
+
+    const handleCopy = () => {
+        console.log("clear text");
+        let newText = document.getElementById("myBox");
+        newText.select();
+        navigator.clipboard.writeText(newText.value);
+    }
+
+    const handleExtraSpaces=()=>{
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "));
+    }
+
     const handleOnChange =(event) =>{
         console.log("handle change")
         setText(event.target.value);
@@ -30,7 +49,9 @@ export default function TextForm(props) {
             </div>
             <button className="btn btn-primary" onClick={handleUpClick} >convert to uppercase</button>
             <button className="btn btn-primary mx-2" onClick={handleLoClick} >convert to Lowercase</button>
-         </div>
+            <button className="btn btn-primary mx-2" onClick={handleclrClick} >Clear text</button>
+            <button className="btn btn-primary mx-2" onClick={handleCopy} >Copy Text</button>
+            <button className="btn btn-primary mx-2" onClick={handleExtraSpaces} >Remove Extra spaces</button>         </div>
          <div className="container my-3">
              <h1>Your Text summary</h1>
              <p>{text.split(" ").length} words and {text.length} characters</p>
